5809fab069edef8c91ce31dcdfb4d795e0049515
Adam Petrovic <adam@freelancer.com>
2013-12-19 14:31:18 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='heisenberg',
+    version='0.1',
+    description='A utility for searching and connecting to EC2 instances',
+    author='Adam Petrovic',
+    author_email='adam@freelancer.com',
+    url='https://gitlab.freelancer.com/adam/heisenberg',
+    packages=[
+        'heisenberg',
+    ],
+    scripts=['bin/ec2'],
+    package_dir={'heisenberg': 'heisenberg'},
+    include_package_data=True,
+    install_requires=[
+        'boto==2.20.1',
+    ],
+    zip_safe=False,
+    keywords='heisenberg',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
